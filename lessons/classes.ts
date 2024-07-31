30745020a560266d60ce991137145567a4265c8a
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-07-31 11:30:59 +0500

    Explore classes in ts

---
+++ lessons/classes.ts
@@ -0,0 +1,32 @@
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+console.log(invOne, invTwo);
+
+let invoices: Invoice[] = [];
+// invoices.push('hello');
+// invoices.push({ name: 'shaun' });
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+
+console.log(invoices);
