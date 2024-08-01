eeee6bf552a76f37726cfcbd5b77c34eb2ade975
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-08-01 14:10:39 +0500

    Modify EventListener: Create new objects based on Invoice/Payment classes, make sure each object implements HasFormatter interface

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,18 @@
 import { Invoice } from './classes/invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-let invoices: Invoice[] = [];
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing work', 200);
 
-invoices.push(invOne);
-invoices.push(invTwo);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
 
-// invOne.client = 'yoshi';
-// invOne.details = 'something else';
-invTwo.amount = 400;
-
-invoices.forEach(inv => {
-  console.log(inv.client, inv.amount, inv.format());
-  // console.log(inv.details);
-})
+// console.log(docs);
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
@@ -29,10 +26,12 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
-  console.log(
-    type.value,
-    tofrom.value,
-    details.value,
-    amount.valueAsNumber
-  );
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  console.log(doc);
 });
