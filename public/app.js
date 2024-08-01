cfa8c759d4c0272fec7d83a77e4ec80109294e9e
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-08-01 14:11:00 +0500

    Compile the code, test it in browser

--- public/app.js
+++ public/app.js
@@ -1,16 +1,13 @@
 import { Invoice } from './classes/invoice.js';
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-// invOne.client = 'yoshi';
-// invOne.details = 'something else';
-invTwo.amount = 400;
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-    // console.log(inv.details);
-});
+import { Payment } from './classes/Payment.js';
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing work', 200);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 // inputs
@@ -20,5 +17,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
