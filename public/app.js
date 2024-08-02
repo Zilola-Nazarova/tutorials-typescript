04f2d8bdac5b7383267266fe2223ed937d29572a
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-08-02 10:18:01 +0500

    Compile the code

--- public/app.js
+++ public/app.js
@@ -1,13 +1,6 @@
-import { Invoice } from './classes/invoice.js';
+import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 // inputs
@@ -15,6 +8,9 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -24,5 +20,6 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    // console.log(doc);
+    list.render(doc, type.value, 'end');
 });
